25c28c2c7b5f4bbe4a42486cf8f385c6d626925f
Ninad Naik <86565419+ninadnaik10@users.noreply.github.com>
2023-05-26 01:13:33 +0530

    Remove dio and solve issue #2

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,36 +1,56 @@
-import 'package:dio/dio.dart';
-import 'package:news_forecast/constants/newsapiurl.dart';
+import 'dart:convert';
+
+// import 'package:dio/dio.dart';
+// import 'package:news_forecast/constants/newsapiurl.dart';
 import 'package:news_forecast/constants/key.dart';
-import 'package:dio_http_cache/dio_http_cache.dart';
+// import 'package:dio_http_cache/dio_http_cache.dart';
+import 'package:http/http.dart' as http;
+// import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NewsService {
-  late DioCacheManager _dioCacheManager;
-  //
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: NewsApiUrl.baseUrl,
-      responseType: ResponseType.json,
-    ),
-  );
+  // late DioCacheManager _dioCacheManager;
+  // //
+  // final Dio _dio = Dio(
+  //   BaseOptions(
+  //     baseUrl: NewsApiUrl.baseUrl,
+  //     responseType: ResponseType.json,
+  //   ),
+  // );
 
   fetchNews() async {
-    _dioCacheManager = DioCacheManager(CacheConfig());
-    Options _cacheOptions =
-        buildCacheOptions(const Duration(days: 7), forceRefresh: true);
-    _dio.interceptors.add(_dioCacheManager.interceptor);
+    // _dioCacheManager = DioCacheManager(CacheConfig());
+    // Options _cacheOptions =
+    //     buildCacheOptions(const Duration(days: 7), forceRefresh: true);
+    // _dio.interceptors.add(_dioCacheManager.interceptor);
 
-    var response = await _dio.get(
-        'v2/top-headlines?apiKey=$API_KEY&category=technology&language=en',
-        options: _cacheOptions);
+    // var response = await _dio.get(
+    //     'v2/top-headlines?apiKey=$API_KEY&category=technology&language=en',
+    //     options: _cacheOptions);
+    const newsApiUrl = "https://newsapi.org/v2/top-headlines?apiKey=$API_KEY&category=technology&language=en";
+    // final cacheManager = DefaultCacheManager();
+    // final file = await cacheManager.getSingleFile(newsApiUrl);
 
-    return response.data;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final connectivityResult = await (Connectivity().checkConnectivity());
+
+    if(connectivityResult == ConnectivityResult.none){
+      // final jsonString = cacheManager.getFileFromCache(newsApiUrl);
+      final results = prefs.getString(newsApiUrl);
+      return jsonDecode(results!);
+    }
+    var response = await http.get(Uri.parse(newsApiUrl));
+    var results = jsonDecode(response.body);
+    // await cacheManager.putFile(newsApiUrl, Uint8List.fromList(utf8.encode(jsonEncode(response.body))));
+    await prefs.setString(newsApiUrl, response.body);
+    print(results);
+    return results;
   }
 
   // fetchNews() async {
   //   const key = 'https://newsapi.org/v2/top-headlines?apiKey=$API_KEY&category=technology&language=en';
-  //   print("helwifjwreh");
   //   final cachedData = await DefaultCacheManager().getSingleFile(key);
-  //   print("helwifjwreh");
   //   if (cachedData != null) {
   //     return json.decode(await cachedData.readAsString());
   //   }
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,39 +1,62 @@
 import 'dart:convert';
 
-import 'package:dio/dio.dart';
-import 'package:dio_http_cache/dio_http_cache.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+// import 'package:dio/dio.dart';
+// import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:news_forecast/constants/key.dart';
 import 'package:news_forecast/services/location_services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherServices {
-  late DioCacheManager _dioCacheManager;
-
-  final Dio _dio = Dio(
-    BaseOptions(
-      responseType: ResponseType.json,
-    ),
-  );
+  // late DioCacheManager _dioCacheManager;
+  //
+  // final Dio _dio = Dio(
+  //   BaseOptions(
+  //     responseType: ResponseType.json,
+  //   ),
+  // );
 
   getWeatherData() async {
+    final locationData = await LocationServices().getCity();
+    final weatherUrl = "https://api.weatherapi.com/v1/current.json?q=$locationData&key=$WEATHER_API_KEY";
+    // final cacheManager = DefaultCacheManager();
+    // final file = await cacheManager.getSingleFile(newsApiUrl);
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final connectivityResult = await (Connectivity().checkConnectivity());
+
+    if(connectivityResult == ConnectivityResult.none){
+      final results = prefs.getString(weatherUrl);
+      return jsonDecode(results!);
+    }
+    var response = await http.get(Uri.parse(weatherUrl));
+    var results = jsonDecode(response.body);
+    await prefs.setString(weatherUrl, response.body);
+    // print(results);
+    return results;
+
+
+
     // final queryParams = {
     //   'key' : WEATHER_API_KEY,
     //   'q' : place
     // };
     // final weatherURL = Uri.http();
-    _dioCacheManager = DioCacheManager(CacheConfig());
-    Options _cacheOptions = buildCacheOptions(const Duration(days: 2), forceRefresh: true);
-    _dio.interceptors.add(_dioCacheManager.interceptor);
-    // print("here");
-    final locationData = await LocationServices().getCity();
-    // print(locationData);
-    var response = await _dio.get(
-      'https://api.weatherapi.com/v1/current.json?q=$locationData&key=$WEATHER_API_KEY', options: _cacheOptions
-    );
-    if (response.statusCode == 200) {
-      // print(response.data);
-      return response.data;
-    } else {
-      throw Exception("Can not get weather");
-    }
+    // _dioCacheManager = DioCacheManager(CacheConfig());
+    // Options _cacheOptions = buildCacheOptions(const Duration(days: 2), forceRefresh: true);
+    // _dio.interceptors.add(_dioCacheManager.interceptor);
+    // // print("here");
+    // final locationData = await LocationServices().getCity();
+    // // print(locationData);
+    // var response = await _dio.get(
+    //   'https://api.weatherapi.com/v1/current.json?q=$locationData&key=$WEATHER_API_KEY', options: _cacheOptions
+    // );
+    // if (response.statusCode == 200) {
+    //   // print(response.data);
+    //   return response.data;
+    // } else {
+    //   throw Exception("Can not get weather");
+    // }
   }
 }
